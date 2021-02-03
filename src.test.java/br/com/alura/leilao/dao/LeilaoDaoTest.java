2855805694c87131788cabb49ee7cb47c6200f17
ttatiane <49352132+ttatiane@users.noreply.github.com>
2021-02-03 13:47:18 -0300

    add: testa métodos simples de escrita que não possuem regra de negócio implementados

---
+++ src.test.java/br/com/alura/leilao/dao/LeilaoDaoTest.java
@@ -0,0 +1,68 @@
+package br.com.alura.leilao.dao;
+
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+class LeilaoDaoTest {
+
+    private LeilaoDao dao;
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.dao = new LeilaoDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
+
+
+    @Test
+    void deveriaCadastrarUmLeilao() {
+        Usuario usuario = criarUsuario();
+        Leilao leilao = new Leilao("Mochila", new BigDecimal("70"), LocalDate.now(), usuario);
+
+        leilao = dao.salvar(leilao);
+
+        Leilao leilaoSalvo = dao.buscarPorId(leilao.getId());
+        Assert.assertNotNull(leilaoSalvo);
+    }
+
+    @Test
+    void deveriaAtualizarUmLeilao() {
+        Usuario usuario = criarUsuario();
+        Leilao leilao = new Leilao("Mochila", new BigDecimal("70"), LocalDate.now(), usuario);
+
+        leilao = dao.salvar(leilao);
+
+        leilao.setNome("Celular");
+        leilao.setValorInicial(new BigDecimal("400"));
+
+        leilao = dao.salvar(leilao);
+
+        Leilao leilaoSalvo = dao.buscarPorId(leilao.getId());
+        Assert.assertEquals("Celular", leilaoSalvo.getNome());
+        Assert.assertEquals(new BigDecimal("400"), leilaoSalvo.getValorInicial());
+    }
+
+    private Usuario criarUsuario() {
+        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+        em.persist(usuario);
+        return usuario;
+    }
+
+}
